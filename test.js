d4d76d9356b864e871640fd1c65798182fc5692e
Hassan <Hassansaleem9@hotmail.com>
2019-08-22 21:19:58 +0500

    Minor changes

---
+++ test.js
@@ -0,0 +1,21 @@
+var fs = require('fs');
+var https = require('https');
+fs.writeFile(__dirname + "/index.html",
+                         "<h1>HTML is great</h1>",
+                          ((error) =>
+    {
+        if (error) {
+            return console.log(error);            
+        } else {
+            return console.log("Congrats");            
+        }
+    })
+ );
+
+
+ var myPhotoLocation = 'https://raw.githubusercontent.com/LearnWebCode/travel-site-files/master/app/assets/images/first-trip-hi-dpi-i.jpg';
+ https.get(myPhotoLocation,((response) => {
+     response.pipe(fs.createWriteStream(__dirname + "/bigPic.jpg"));
+
+ })
+ );
